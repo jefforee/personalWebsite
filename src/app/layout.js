d519ac043cd97f9c4e97df356b94f55eec6fde20
Jeffrey Huang <jeffreyhuang2002@gmail.com>
2024-10-01 16:55:01 -0700

    added icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,11 +4,16 @@ import { HasStartedProvider, useHasStarted } from "./contexts/HasStartedContext"
 import NavBar from "./components/Navbar";
 import Footer from "./components/Footer";
 import "./styles/globals.css";
+import Head from 'next/head';
 
 export default function RootLayout({ children }) {
   return (
     <HasStartedProvider>
       <html lang="en" className="scroll-smooth">
+        <Head>
+          <link rel="icon" href="./favicon.ico" sizes="any" />
+        </Head>
+
         <body className="bg-[#E0E0DB]">
           <NavBar />
           <div className="w-full px-[10%] lg:px-60">
